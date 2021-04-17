4a6876e139969930618ee9849485a7ccdaaf700d
Renan Santana <renan_ota@hotmail.com>
2021-04-17 04:19:42 -0300

    Setup som e detalhes na pista

--- mini-game/mini-game.ino
+++ mini-game/mini-game.ino
@@ -7,6 +7,7 @@
 #define PLAYER_WIDTH 8
 #define PLAYER_HEIGHT 8
 #define PLAYER_Y_POSITION 50
+#define BUZZER_PIN 18
 
 static const unsigned char PROGMEM player[] = {
   B00011000,
@@ -21,15 +22,18 @@ static const unsigned char PROGMEM player[] = {
 
 float potentiometer_read;
 int player_position;
+int score = 0;
+unsigned long last_millis = 0;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void setup() 
+
+void setup()
 {
   Serial.begin(9600);
-  
+
   // Setup display
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
@@ -38,29 +42,40 @@ void setup()
   display.display();
   delay(1000);
   display.clearDisplay();
+
+  // Setup buzzer
+  //ledcSetup(0, 5000, 10);
+  //ledcAttachPin(BUZZER_PIN, 0);
+  //ledcWriteTone(0, 5000);
 }
 
-void loop() 
+
+void loop()
 {
   // Read input
   potentiometer_read = analogRead(A0);
-  
+
   // Calc player position
   player_position = (int)map(potentiometer_read, 4095, 0, 0, SCREEN_WIDTH - PLAYER_WIDTH - 1);
+  // Limita a posição do player dentro da pista
+  player_position = constrain(player_position, 10, display.width()-1-PLAYER_WIDTH-10);
   display.clearDisplay();
-  
+
   // Draw player
   // display.drawPixel(player_position, 63, WHITE);
   display.drawBitmap(player_position, PLAYER_Y_POSITION, player, PLAYER_WIDTH, PLAYER_HEIGHT, WHITE);
 
-  // Draw circuit
+  // Draw extern road line
   display.drawLine(30, 16, 0, display.height()-1, WHITE);
   display.drawLine(display.width()-30-1, 16, display.width()-1, display.height()-1, WHITE);
+  // Draw intern road line
+  display.drawLine(31, 16, 0, display.height()-1+6, WHITE);
+  display.drawLine(display.width()-31-1, 16, display.width()-1, display.height()-1+6, WHITE);
 
   // Draw Score
-  display.setCursor(64, 0);
+  display.setCursor(0, 0);
   display.print("Score: 000");
-  
+
   // Render the buffer
   display.display();
   delay(15);
